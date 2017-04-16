8b954294c8427902f6827569d96f3dc1624dcacf
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-04-16 17:21:37 +0300

    Add basic stuff for gem

--- test/surf_test.rb
+++ test/surf_test.rb
@@ -4,8 +4,4 @@ class SurfTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Surf::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,16 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'surf'
 
 require 'minitest/autorun'
+
+require "minitest/autorun"
+require "minitest/pride"
+require "minitest/mock"
+require "minitest/hell"
+
+class Minitest::Test
+  parallelize_me!
+end
+
+module Kernel
+  alias_method :context, :describe
+end
