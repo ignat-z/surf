32e2a4c27e9e11980589e7ba9a899cc5de322263
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-04-21 23:22:24 +0300

    Surf runner

--- lib/surf.rb
+++ lib/surf.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 
+require 'puma'
+
+require 'surf/utils/router'
 require 'surf/version'
 
 module Surf
-  # Your code goes here...
+  def self.run
+    conifg = OpenStruct.new(host: 'localhost', port: 9001)
+    app = Surf::Router.new([])
+    @server_thread = Thread.new { run_app(app, conifg) }
+    @server_thread.abort_on_exception = true
+  end
+
+  def self.run_app(app, conifg)
+    @server = Puma::Server.new(app)
+    begin
+      @server.add_tcp_listener(conifg.host, conifg.port)
+    rescue Errno::EADDRINUSE, Errno::EACCES => exception
+      handle_error(exception)
+    end
+    @server.run
+  end
+
+  def self.handle_error(exception)
+    p exception
+    abort
+  end
 end
