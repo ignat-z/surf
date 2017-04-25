703ef2f3d537fbbcf6c16afbcdf0c0bb8c8228bd
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-04-26 00:31:20 +0300

    Test on Surf.configure

--- test/surf_test.rb
+++ test/surf_test.rb
@@ -3,7 +3,20 @@
 require 'test_helper'
 
 describe Surf do
+  subject do
+    Surf.dup
+  end
+
   it 'has_a_version_number' do
     refute_nil ::Surf::VERSION
+  end
+
+  context '.configure' do
+    it 'yields the Configuration object' do
+      subject.configure do |config|
+        config.port = 27
+      end
+      assert_equal 27, subject.port
+    end
   end
 end
