b3ff6735ad290dbd463a8ddcd83aba8f5ddf51a3
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-04-28 08:13:25 +0300

    Change Surf module methods signature

--- lib/surf.rb
+++ lib/surf.rb
@@ -10,35 +10,37 @@ require 'surf/version'
 
 module Surf
   extend SingleForwardable
-  def_delegators :configuration, :check_configuration!, *Configuration.attributes.map(&:name)
+  def_delegators :configuration, *Configuration.attributes.map(&:name)
 
-  def self.run
-    app = Surf::Router.new([Surf::WebhookHandler])
-    @server_thread = Thread.new { run_app(app) }
-    @server_thread.abort_on_exception = true
-  end
-
-  def self.run_app(app)
-    @server = Puma::Server.new(app)
-    begin
-      @server.add_tcp_listener(Surf.host, Surf.port)
-    rescue Errno::EADDRINUSE, Errno::EACCES => exception
-      handle_error(exception)
+  class << self
+    def run
+      app = Surf::Router.new([Surf::WebhookHandler])
+      @server_thread = Thread.new { run_app(app) }
+      @server_thread.abort_on_exception = true
     end
-    @server.run
-  end
 
-  def self.handle_error(exception)
-    Surf.logger.fatal(exception)
-    abort
-  end
+    def run_app(app)
+      @server = Puma::Server.new(app)
+      begin
+        @server.add_tcp_listener(Surf.host, Surf.port)
+      rescue Errno::EADDRINUSE, Errno::EACCES => exception
+        handle_error(exception)
+      end
+      @server.run
+    end
 
-  def self.configure
-    yield configuration
-  end
+    def handle_error(exception)
+      Surf.logger.fatal(exception)
+      abort
+    end
 
-  def self.configuration
-    @configuration ||= Surf::Configuration.new
+    def configure
+      yield configuration
+    end
+
+    def configuration
+      @configuration ||= Surf::Configuration.new
+    end
   end
   private_class_method :configuration
 end
