b89e383a20569097edbd1947208c3aaca125554a
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-05-01 23:19:05 +0300

    Move config to config path

--- lib/surf/cli.rb
+++ lib/surf/cli.rb
@@ -7,10 +7,11 @@ require 'surf/config_builder'
 
 module Surf
   class CLI < Thor
+    DEFAULT_CONFIG_PATH = 'config/default_config.yml'
     default_task :start
 
     desc 'start', 'Starts Surf'
-    def start(config = 'default_config.yml')
+    def start(config = DEFAULT_CONFIG_PATH)
       config = File.read(config)
       Surf::ConfigBuilder.new(config).build!
       Surf.run
